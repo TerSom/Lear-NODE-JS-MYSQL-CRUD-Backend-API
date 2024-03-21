19ebb7d86d738e6f5bae35c6753ffb2b24af913d
TerSom <140949512+TerSom@users.noreply.github.com>
2024-03-21 22:00:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send("utama")
 })
 
 app.get('/hello', (req, res) => {
+  console.log({urlParam: req.query.lamat})
   res.send('Hello World!')
 })
 
 app.post('/login', (req,res) => {
+  console.log({ requestFromOutside: req.body })
     res.send("login berhasil")
+})
+
+app.put('/username', (req, res) => {
+  console.log({ updateData: req.body})
+  res.send('update berhasils')
 })
 
 app.listen(port, () => {
